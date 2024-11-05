4e68974911d33ed5089d14ead316ef1ad6a56db9
Navin-VijayaKumar <navinv10122004@gmail.com>
2024-11-05 22:09:02 +0530

    commit seven

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,99 +1,74 @@
-import React from 'react'
-import './Home.css'
-import reef from './Assertsp/reef.jpg'
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+import reef from './Assertsp/reef.jpg';
+import pond from './Assertsp/pond.jpg';
+import r1 from './Assertsp/r1.jpg';
+import slidern1 from './Assertsp/slidern1.jpg';
+import slidern2 from './Assertsp/slidern2.jpg';
+
 const Home = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const images = [reef, pond, r1, slidern1, slidern2]; 
+  const texts = [
+    { title: "Coral Reef Ecosystem", description: "Explore the vibrant life within coral reefs." },
+    { title: "Beautiful Pond", description: "Discover the tranquility of a serene pond." },
+    { title: "Rock Scape", description: "Witness the colorful underwater Rock world..." },
+    { title: "Coral Reef and Rock Ecosystem", description: "Relax by the peaceful Rock Ecosystem view." },
+    { title: "Planted Tank Make You feel alive", description: "A Piece of Nature Inside Your House" }
+  ]; 
+  const nextImage = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextImage, 5000); 
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleButtonClick = (direction) => {
+    let newIndex = activeIndex + (direction === 'next' ? 1 : -1);
+    if (newIndex < 0) newIndex = images.length - 1;
+    if (newIndex >= images.length) newIndex = 0;
+    setActiveIndex(newIndex);
+  };
+
   return (
-    <div className="container-1">
-
-
-    <div className="list-1">
-      <div className="items-1">
-        <img src={reef} alt=''></img>
-        <div className="content-1">
-          <div className="items00"><p>fish</p></div>
-          <div className="items11"><p>planted</p></div>
-          <div className="items22"><p>full</p></div>
-          <div className="des">
-            <p>riiiiiiiiiiiiiiiicniorcceinnnnnn</p>
-          </div>
-          <div className="button-1">
-            <button>See More</button>
-          </div>
-        </div>
-      </div>
-      <div className="items-1">
-        <img src={reef} alt=''></img>
-        <div className="content-1">
-          <div className="items00"><p>fish</p></div>
-          <div className="items11"><p>planted</p></div>
-          <div className="items22"><p>full</p></div>
-          <div className="des">
-            <p>riiiiiiiiiiiiiiiicniorcceinnnnnn</p>
-          </div>
-          <div className="button-1">
-            <button>See More</button>
-          </div>
-        </div>
-      </div>
-      <div className="items-1">
-        <img src={reef} alt=''></img>
-        <div className="content-1">
-          <div className="items00"><p>fish</p></div>
-          <div className="items11"><p>planted</p></div>
-          <div className="items22"><p>full</p></div>
-          <div className="des">
-            <p>riiiiiiiiiiiiiiiicniorcceinnnnnn</p>
-          </div>
-          <div className="button-1">
-            <button>See More</button>
-          </div>
-        </div>
-      </div>
-      <div className="items-1">
-        <img src={reef} alt=''></img>
-        <div className="content-1">
-          <div className="items00"><p>fish</p></div>
-          <div className="items11"><p>planted</p></div>
-          <div className="items22"><p>full</p></div>
-          <div className="des">
-            <p>riiiiiiiiiiiiiiiicqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqniorcceinnnnnn</p>
-          </div>
-          <div className="button-1">
-            <button>See More</button>
-          </div>
-        </div>
-      </div>
-      <div className="items-1">
-        <img src={reef} alt=''></img>
-        <div className="content-1">
-          <div className="items00"><p>fish</p></div>
-          <div className="items11"><p>planted</p></div>
-          <div className="items22"><p>full</p></div>
-          <div className="des">
-            <p>riiiiiiiiiiiiiiiicniorcceinnnnnn</p>
-          </div>
-          <div className="button-1">
-            <button>See More</button>
-          </div>
-        </div>
-      </div>
-      <div className="items-1">
-        <img src={reef} alt=''></img>
-        <div className="content-1">
-          <div className="items00"><p>fish</p></div>
-          <div className="items11"><p>planted</p></div>
-          <div className="items22"><p>full</p></div>
-          <div className="des">
-            <p>riiiiiiiiiiiiiiiicniorccenn</p>
-          </div>
-          <div className="button-1">
-            <button>See More</button>
-          </div>
-        </div>
+    <div className="carousal-all">
+      <div className="carousel" data-carousel>
+        <button
+          className="button1n"
+          onClick={() => handleButtonClick('prev')}
+          data-carousel-button="prev"
+        >
+          &#8656;
+        </button>
+        <button
+          className="button2n"
+          onClick={() => handleButtonClick('next')}
+          data-carousel-button="next"
+        >
+          &#8658;
+        </button>
+        <ul data-slides>
+          {images.map((image, index) => (
+            <li
+              key={index}
+              className={`slider1 ${index === activeIndex ? 'active' : ''}`}
+              data-active={index === activeIndex ? true : null}
+            >
+              <img src={image} alt={`Slide ${index + 1}`} />
+              {index === activeIndex && ( // Display text only for the active slide
+                <div className="slide-text">
+                  <h1>{texts[index].title}</h1>
+                  <p>{texts[index].description}</p>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
