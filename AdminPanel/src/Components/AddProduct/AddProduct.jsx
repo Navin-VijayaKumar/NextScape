eea61844df868fed70cc75e63af24daf1f633ae2
Navin-VijayaKumar <navinv10122004@gmail.com>
2024-11-25 20:20:38 +0530

    commit eightteen

--- AdminPanel/src/Components/AddProduct/AddProduct.jsx
+++ AdminPanel/src/Components/AddProduct/AddProduct.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './AddProduct.css'
 const AddProduct = () => {
+  // id: Number,
+  // image: String,
+  // DealerName: String,
+  // category: String,
+  // litter: String,
+  // PhoneNumber: String,
+  // Email: String,
+  // address: String,
+  // state: String,
+  // District: String,
+  // Dimensions: String,
+  // decorationLevel: String,
+  // filterationType: String,
+  // price: String
+
+
+
+
+
+
   return (
-    <div>AddProduct</div>
+    <div className="AddProduct">
+      <div className="AddProductTitle">
+        <h1>Add Product</h1>
+      </div>
+      <div className="AddProductForm">
+        <p>123</p>
+        <input type='number' placeholder=''></input>
+
+      </div>
+    </div>
   )
 }
 
